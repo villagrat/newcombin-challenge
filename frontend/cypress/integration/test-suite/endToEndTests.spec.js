9089320bebeffb17305f1e2ede2bfc61470be432
villagrat <elsainetedelautrec@gmail.com>
2022-04-12 01:39:48 -0300

    navigate to about test

--- frontend/cypress/integration/test-suite/endToEndTests.spec.js
+++ frontend/cypress/integration/test-suite/endToEndTests.spec.js
@@ -18,17 +18,19 @@ describe('Requisitos de la App', () => {
   });
 
   it.only('La Mock Data del server para Cosme Fulanito se carga correctamente en el estado inicial', () => {
-    //
-    cy.get('.person').should('have.length', 1);
-
-    //
-    cy.get('.person > :nth-child(1)').should('have.text', 'Cosme');
-    cy.get('.person > :nth-child(2)').should('have.text', 'Fulanito');
-    cy.get('.person > :nth-child(3)').should(
-      'have.text',
+    cy.get('.lista-container > :nth-child(3)').should('contains.text', 'Cosme');
+    cy.get('.lista-container > :nth-child(3)').should(
+      'contains.text',
+      'Fulanito'
+    );
+    cy.get('.lista-container > :nth-child(3)').should(
+      'contains.text',
       '742 Evergreen Terrance'
     );
-    cy.get('.person > :nth-child(4)').should('have.text', '333-22-4444');
+    cy.get('.lista-container > :nth-child(3)').should(
+      'contains.text',
+      '333-22-4444'
+    );
   });
 
   it.only('El botton de submit se habilita sólo cuando el form se llena con información correcta', () => {
@@ -69,28 +71,24 @@ describe('Requisitos de la App', () => {
     // Submit Form
     cy.get('form').submit();
     // Check that the item has been created and is now displayed
-    cy.get('.lista-container > :nth-child(4) > :nth-child(1) ').should(
-      'have.text',
-      'Pedro'
-    );
-    cy.get('.lista-container > :nth-child(4) > :nth-child(2)').should(
-      'have.text',
-      'Gomez'
-    );
-    cy.get(':nth-child(4) > :nth-child(3)').should(
-      'have.text',
+    cy.get('.lista-container > :nth-child(4)').should('contain.text', 'Pedro');
+    cy.get('.lista-container > :nth-child(4)').should('contain.text', 'Gomez');
+    cy.get('.lista-container > :nth-child(4)').should(
+      'contain.text',
       'CalleFalsa 123'
     );
-    cy.get(':nth-child(4) > :nth-child(4)').should('have.text', '123-12-1234');
+    cy.get('.lista-container > :nth-child(4)').should(
+      'contain.text',
+      '123-12-1234'
+    );
   });
 
-  it.only('No se puede agregar a una persona que tenga un SSN ya existente a la lista', () => {
-    // We'll store our item text in a variable so we can reuse it
+  it.only('Aviso de carga exitosa con popup', () => {
     const newPersonaItem = {
-      firstName: 'MismoSSN',
-      lastName: 'QueCosmeFulanito',
-      address: 'CalleFalsa 456',
-      ssn: '333-22-4444',
+      firstName: 'NewGuy',
+      lastName: 'NewLast',
+      address: 'CalleFalsa 789',
+      ssn: '321-12-4321',
     };
 
     // Input data into corresponding form fields
@@ -102,10 +100,13 @@ describe('Requisitos de la App', () => {
     // Submit Form
     cy.get('form').submit();
     // A toastify pop-up with the correct error message shows up
-    cy.get('.Toastify__toast-body').should('contains.text', 'Duplicate SSN');
+    cy.get('.Toastify__toast-body').should(
+      'contains.text',
+      'Persona agregada exitosamente'
+    );
   });
 
-  it.only('El boton reset debe limpiar los campos del formulario', () => {
+  it.only('El boton reset debe vaciar los campos del formulario', () => {
     // cargamos el estado del form con data
     const newPersonaItem = {
       firstName: 'MismoSSN',
@@ -128,5 +129,51 @@ describe('Requisitos de la App', () => {
     cy.get('#lastName').should('have.value', '');
     cy.get('#address').should('have.value', '');
     cy.get('#ssn').should('have.value', '');
+  });
+
+  it.only('Navegar a la pagina About funciona correctamente', () => {
+    // Clickear About en la Navbar
+    cy.get(':nth-child(2) > a').click();
+    cy.location('pathname').should('match', /\/about$/);
+  });
+
+  it.only('No se puede agregar a una persona que tenga un SSN ya existente a la lista', () => {
+    const newPersonaItem = {
+      firstName: 'MismoSSN',
+      lastName: 'QueCosmeFulanito',
+      address: 'CalleFalsa 456',
+      ssn: '333-22-4444',
+    };
+
+    // Input data into corresponding form fields
+    // https://on.cypress.io/selecting-elements
+    cy.get('#firstName').type(`${newPersonaItem.firstName}{enter}`);
+    cy.get('#lastName').type(`${newPersonaItem.lastName}{enter}`);
+    cy.get('#address').type(`${newPersonaItem.address}{enter}`);
+    cy.get('#ssn').type(`${newPersonaItem.ssn}{enter}`);
+    // Submit Form
+    cy.get('form').submit();
+    // A toastify pop-up with the correct error message shows up
+    cy.get('.Toastify__toast-body').should('contains.text', 'Duplicate SSN');
+  });
+
+  it.only('Aviso de errores con popup', () => {
+    const newPersonaItem = {
+      firstName: 'MismoSSN',
+      lastName: 'QueCosmeFulanito',
+      address: 'CalleFalsa 456',
+      ssn: '333-22-4444',
+    };
+
+    // Input data into corresponding form fields
+    // https://on.cypress.io/selecting-elements
+    cy.get('#firstName').type(`${newPersonaItem.firstName}{enter}`);
+    cy.get('#lastName').type(`${newPersonaItem.lastName}{enter}`);
+    cy.get('#address').type(`${newPersonaItem.address}{enter}`);
+    cy.get('#ssn').type(`${newPersonaItem.ssn}{enter}`);
+    // Submit Form
+    cy.get('form').submit();
+    // A toastify pop-up with the correct error message shows up
+    cy.get('.Toastify__toast-body').should('contains.text', 'Duplicate SSN');
   });
 });
